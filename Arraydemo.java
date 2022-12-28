001f8644c73664a3f37bb12812d1905b8b5e7605
Sahil9644 <120358533+Sahil9644@users.noreply.github.com>
2022-12-28 10:44:50 +0530

    Add files via upload

---
+++ Arraydemo.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Arraydemo {
+
+	public static void main(String[] args) {
+		int arr[]={1,2,3,4,5};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}// TODO Auto-generated method stub
+
+	}
+
+}
